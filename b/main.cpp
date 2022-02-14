4d444e8423ec917a592da88b0c74b82cc37c3d38
Ahmad1321 <92415089+Ahmad1321@users.noreply.github.com>
2022-02-14 21:31:09 +0700

    Update main.cpp

--- b/main.cpp
+++ b/main.cpp
@@ -3,17 +3,21 @@ using namespace std;
 
 int main(){
 	
+	//Menggunakan construct
 	data a("Real Madrid", "Spanyol", 1902, 25);
 	
+	//Print output
 	a.output();
 	
 	cout << endl;
 	
+	//Menggunakan set
 	data b;
 	b.settim("Liverpool");
 	b.setnegara("England");
 	b.settahun(1892);
 	b.setjumlah(34);
 	
+	//Print Output
 	b.output();
 }
