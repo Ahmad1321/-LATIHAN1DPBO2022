382771401ca8524df176a4c08d72772a036a2a52
Ahmad1321 <92415089+Ahmad1321@users.noreply.github.com>
2022-02-14 19:51:09 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        // menggunakan construct
+        data a = new data("Real Madrid", "Spanyol", 1902, 25);
+		a.printOut();
+        System.out.println();
+		
+        // menggunakan set
+        data b = new data();
+        b.settim("Liverpool");
+        b.setnegara("England");
+        b.settahun(1892);
+        b.setjumlah(34);
+
+        b.printOut();
+    }
+}
